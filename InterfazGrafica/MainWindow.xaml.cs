a99f1f213e839d75716d7450570a834d496b2ec7
Godiebarbosa <godiebarbosa@gmail.com>
2024-09-04 18:46:44 -0500

    Tercer Commit

---
+++ InterfazGrafica/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+namespace MiAplicacionWPF
+{
+ public partial class MainWindow : Window
+ {
+ public MainWindow()
+ {
+ InitializeComponent();
+ }
+ private void button1_Click(object sender, RoutedEventArgs e)
+ {
+ MessageBox.Show("¡Hola, mundo!");
+ }
+ }
+}
